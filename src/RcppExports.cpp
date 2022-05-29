c341d102404afae33e4ce02d0803a81672624d24
Jeroen Ooms <jeroenooms@gmail.com>
2022-05-29 10:09:04 +0200

    Donttest examples to comply with CRAN offline policy

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // cpp_pdf_length
 int cpp_pdf_length(char const* infile, char const* password);
 RcppExport SEXP _qpdf_cpp_pdf_length(SEXP infileSEXP, SEXP passwordSEXP) {
